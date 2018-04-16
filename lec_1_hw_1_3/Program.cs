f513960188f14fcad77dceb17ab00bdf5bd706b3
PavelSheglov <tpaul-1981@yandex.ru>
2018-04-16 22:35:01 +0700

    Задача по обнулению старшего бита числа

--- lec_1_hw_1_3/Program.cs
+++ lec_1_hw_1_3/Program.cs
@@ -10,6 +10,18 @@ namespace lec_1_hw_1_3
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.Write("Введите целое число (от {0} до {1}):", ushort.MinValue, ushort.MaxValue);
+                ushort num = Convert.ToUInt16(Console.ReadLine());
+                const ushort mask = 1;
+                int size = sizeof(ushort) * 8;
+                Console.WriteLine("Число после обнуления последнего бита = {0}", num & ~(mask << (size - 1)));
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Введено не целое число (либо превышен диапазон)!!!");
+            }
         }
     }
 }
